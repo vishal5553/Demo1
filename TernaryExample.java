8a1b7fc933aff6cc23b2e40b6d5b8c2d16072361
vishal5553 <101632391+vishal5553@users.noreply.github.com>
2022-03-15 16:47:12 +0530

    New file3

---
+++ TernaryExample.java
@@ -0,0 +1,16 @@
+package Basiccommands;
+//wap to  demonstrate ternary opertor
+public class TernaryExample {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=50;
+		//conditional operator /ternary operator 
+		//condition ? true : false 
+		int c=a>b ? a:b;
+		
+		System.out.println(c);
+
+	}
+
+}
---
+++ TestType.java
@@ -0,0 +1,22 @@
+package Basiccommands;
+
+public class TestType {
+  boolean b;
+  int a;
+  float c=90;
+  double d;
+	public static void main(String[] args) 
+	{ 
+	  float r= 89.8f; // by default decimal is considered as double
+	  
+		
+	 TestType t= new TestType();
+	 System.out.println(t.b);
+	 System.out.println(t.a);
+	 System.out.println(t.c);
+	 System.out.println(t.d);
+	 
+	 
+	}
+
+}
